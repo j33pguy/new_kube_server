5fadbec64ed1c4050b8a0d09625388ec78227e56
j33pguy <30570813+j33pguy@users.noreply.github.com>
2021-09-23 21:47:25 -0400

    Update serv_kickoff.sh

--- serv_kickoff.sh
+++ serv_kickoff.sh
@@ -1,4 +1,5 @@
    #!/bin/bash
    
    # curl url to download this script
-   curl -Lo https://github.com/j33pguy/new_kube_server/
+   #curl -Lo https://github.com/j33pguy/new_kube_server/
+echo "YESSIR"
