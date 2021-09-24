d220d636b14f1ed5dfe25676d31154e41ceca0ab
j33pguy <30570813+j33pguy@users.noreply.github.com>
2021-09-24 14:50:59 -0400

    Update serv_kickoff.sh

--- serv_kickoff.sh
+++ serv_kickoff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -LO https://github.com/j33pguy/new_kube_server/raw/main/serv_kickoff.sh && bash serv_kickoff.sh
+curl -LO https://github.com/j33pguy/new_kube_server/raw/main/new_serv.sh && bash new_serv.sh
